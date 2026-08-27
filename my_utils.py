86461ccacd5c888cd74a29ff32526cc7ef0dc72c
Alan Leyk <alle1427@colorado.edu>
2026-08-27 14:04:09 -0600

    fixed column

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,7 @@
 def get_column(file_name, query_column, query_value, result_column):
+    results = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            values=line.split()
+    
     return None
